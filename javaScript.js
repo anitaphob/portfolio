eb22858a68be6ab9686465713e8c1f35254b97c7
Sebi <rotar.seby1@gmail.com>
2018-01-08 04:17:38 +0100

    first commit

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+
+let topLeft = document.getElementById("topLeft");
+/*let topRight = document.getElementById("topRight");
+let bottomLeft = document.getElementById("bottomLeft");
+let bottomRight = document.getElementById("bottomRight");*/
+
+
+
+
+function doShow(click){
+    if(click.style.display != "block")
+    {
+        click.style.display = "block";
+    }
+    else {
+        click.style.display = "none";
+    }
+}
+
+
